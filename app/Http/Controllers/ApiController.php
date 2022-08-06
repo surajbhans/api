bd5124a9d8fe2bc709ec59b62a60d855911df4c2
surajbhans <110710754+surajbhans@users.noreply.github.com>
2022-08-06 11:17:36 +0530

    first commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Api;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return api::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new api();
+        $data->name = $request->name;
+        $data->contact = $request->contact;
+        $data->email = $request->email;
+        $data->save();
+        return ["error"=>"data inserted successfully"];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Api $api)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+  
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Api $api)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Api $api)
+    {
+        //
+    }
+}
